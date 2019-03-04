ce0b2546a76c25e3cb15b173daf813e948bb428c
Shinji Kaneko <shinji@k636174.net>
2019-03-04 22:58:21 +0900

    Initial commit

---
+++ pcm-agnet.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+	hostname, _ := os.Hostname()
+	filename := hostname + ".txt"
+	out, _ := exec.Command("df", "-al").Output()
+	outlines := strings.Split(string(out), "\n")
+	l := len(outlines)
+
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	for _, line := range outlines[1 : l-1] {
+		parsedLine := strings.Fields(line)
+		row := strconv.FormatInt(t.Unix(), 10) + "," + strings.Join(parsedLine, ",")
+		fmt.Fprintln(file, row)
+	}
+}
