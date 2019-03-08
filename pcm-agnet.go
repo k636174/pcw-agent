2187638f857f3963741b98ef1ee1c83425dc7795
Shinji Kaneko <shinji@k636174.net>
2019-03-08 21:23:32 +0900

    change agent(log)

--- pcm-agnet.go
+++ pcm-agnet.go
@@ -14,33 +14,31 @@ func main() {
 	t := time.Now()
 	hostname, _ := os.Hostname()
 	filename := hostname + ".tmp"
-	out, _ := exec.Command("df", "-alk").Output()
-	outlines := strings.Split(string(out), "\n")
-	l := len(outlines)
-
 	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
+	// diskusage
+	out, _ := exec.Command("df", "-alk").Output()
+	outlines := strings.Split(string(out), "\n")
+	l := len(outlines)
 	for _, line := range outlines[1 : l-1] {
 		parsedLine := strings.Fields(line)
 		row := strconv.FormatInt(t.Unix(), 10) + "," + hostname + "," + strings.Join(parsedLine, ",") + ",diskusage"
 		fmt.Fprintln(file, row)
 	}
 
+	// top_result(load_average)
 	out2, _ := exec.Command("sh", "-c", "top -l 1 | head -n 10").Output()
 	outlines2 := strings.Split(string(out2), "\n")
-	l2 := len(outlines2)
-	for _, line := range outlines2[1 : l2-0] {
+	for _, line := range outlines2[1:len(outlines2)] {
 		parsedLine := strings.Split(line, ":")
 		if 1 != len(parsedLine) {
 			parsedLine2 := strings.Split(parsedLine[1], ",")
 			row := strconv.FormatInt(t.Unix(), 10) + "," + hostname + "," + strings.Join(parsedLine2, ",") + "," + parsedLine[0]
 			fmt.Fprintln(file, row)
 		}
-
 	}
-
 }
