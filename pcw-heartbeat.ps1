679fe4f99ce46c21a641df92b12a28bfeeb08d3a
k636174 <k636174@private.k636174.net>
2019-03-18 22:43:41 +0900

    add pcw-heartbeat.ps1

---
+++ pcw-heartbeat.ps1
@@ -0,0 +1,8 @@
+﻿$url="https://pcm.k636174.net/api/heartbeat"
+
+[String] $MyHostname = [Net.Dns]::GetHostName()
+[String] $MyIPs = [system.net.dns]::GetHostAddresses((hostname)) | where {$_.AddressFamily -eq "InterNetwork"} | select -ExpandProperty IPAddressToString
+
+$postParams = @{hostname=$MyHostname; src_lip=$MyIPs}
+
+Invoke-WebRequest $url -Method POST -Body $postParams
